bb7924cdddb6a6acd4cade07897b162ac8bc1a05
huhn <sehe89@gmail.com>
2019-12-04 23:07:55 +0100

    add robot

---
+++ frontend/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <h1 class="title">AKITA Robot</h1>
+    <h2 class="sub-title">IOTA Machine Wallet</h2>
+    <div v-if="!payed">
+      <iota-payment class="pay-button" @paymentSuccess="paymentSuccess">Show QR Code</iota-payment>
+    </div>
+    <div v-else>
+        <img class="robot" src="./assets/Robot002.gif" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data() {
+    return {
+      payed: false
+    }
+  },
+  methods: {
+    paymentSuccess() {
+      this.payed = true;
+
+      setTimeout(function() {
+        // click on button
+        location.reload();
+      }, 10000);
+      
+    }
+  }
+}
+</script>
+
+<style >
+:root {
+  --akita-primary: #00b0f0;
+  --akita-secondary: #00fb92;
+  --akita-dark: #3b3838;
+  --akita-light: #f2f2f2;
+  --akita-blue: #00b0f0;
+}
+body {
+  height: 100vh;
+  widows: 100%;
+  text-align: center;
+  justify-content: center;
+    background: linear-gradient(
+    to bottom right,
+    var(--akita-primary) 50%,
+    var(--akita-secondary) 85%
+  );
+}
+#app {
+  font-family: "Oswald", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: var(--akita-light);
+  margin: 0 auto;
+  margin-top: 60px;
+
+  border-radius: 10px;
+  padding: 20px 20px;
+  max-width: 600px;
+}
+.title {
+  font-size: 4em;
+  font-weight: bold;
+}
+.sub-title {
+  font-size: 3em;
+  font-weight: bold;
+}
+.logo {
+  width: 200px;
+}
+
+.pay-button button {
+  background-color: var(--akita-dark);
+  border-radius: 10px;
+  border: none;
+  color: white;
+  padding: 30px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+}
+.pay-button button:hover {
+  cursor: pointer;
+}
+
+.robot {
+  width: 50%;
+}
+</style>
---
+++ index.js
@@ -0,0 +1,67 @@
+var paymentModule = require('iota-payment')
+var app = require('express')()
+var cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors())
+
+var options = {
+    mount: '/payments',
+    value: 1000,
+    websockets: true
+    // ...
+}
+
+let server = paymentModule.createServer(app, options)
+
+
+//Create an event handler which is called, when a payment was successfull
+var onPaymentSuccess = function (payment) {
+
+    var url = process.env.PROVIDER_URL;
+    var headers = {
+        "Content-Type": "application/json"
+    }
+    var data = {
+        "name": "akita_lego_robot",
+        "description": "Pay for energy"
+    }
+    fetch(url, { method: 'POST', headers: headers, body: data})
+    .then((res) => {
+        return res.json()
+    })
+    .then((json) => {
+        console.log(json);
+        // Do something with the returned data.
+        console.log('payment success!', payment);
+        let payoutObject = {
+            //required
+            address: 'HW99PKRDWBUCCWLEQDONQXW9AXQHZAABYEKWSEFYAUIQWWCLVHAUJEQAZJACAGPZBZSQJIOUXRYYEXWZCXXOAJMZVY', 
+            value: 1, 
+            //optional
+            message: 'Example message',
+            tag: 'TRYTETAG',
+            //indexes for input addresses, only in special cases required
+            // starIndex: 0,
+            // endIndex: 1
+        }
+        paymentModule.payout.send(payoutObject)
+        .then(result => {
+            console.log("result", result)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    });
+   
+
+}
+
+paymentModule.on('payoutSent', onPaymentSuccess);
+
+// Start server with iota-payment module on '/custom'
+server.listen(5000, function () {
+    console.log(`Server started on http://localhost:5000 `)
+})
+

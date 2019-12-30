ff6ab3a1d2651be76a6d4425431349152d7a2866
huhn <sehe89@gmail.com>
2019-12-30 21:00:47 +0100

    fix express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var paymentModule = require('iota-payment')
-var app = require('express')()
+var express = require('express')
+var app = express()
 var cors = require('cors')
 const fs = require('fs');
 const dotenv = require('dotenv');

aa2985bf6d087ad24d59dc00794738825b1bf3ab
huhn <sehe89@gmail.com>
2019-12-30 22:56:15 +0100

    fix paymentSuccess

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express()
 var cors = require('cors')
 const fs = require('fs');
 const dotenv = require('dotenv');
+const axios = require('axios');
+
 dotenv.config();
 
 app.use(cors())
@@ -37,6 +39,9 @@ let server = paymentModule.createServer(app, options)
 //Create an event handler which is called, when a payment was successfull
 var onPaymentSuccess = function (payment) {
 
+    console.log("onPaymentSuccess", payment)
+    console.log("PROVIDER_URL", process.env.PROVIDER_URL)
+
     var url = process.env.PROVIDER_URL;
     var headers = {
         "Content-Type": "application/json"
@@ -45,41 +50,42 @@ var onPaymentSuccess = function (payment) {
         "name": "akita_lego_robot",
         "description": "Pay for energy"
     }
-    fetch(url, { method: 'POST', headers: headers, body: data})
-    .then((res) => {
-        return res.json()
-    })
-    .then((json) => {
-        console.log(json);
-        // Do something with the returned data.
-        console.log('payment success!', payment);
-        let payoutObject = {
-            //required
-            address: 'HW99PKRDWBUCCWLEQDONQXW9AXQHZAABYEKWSEFYAUIQWWCLVHAUJEQAZJACAGPZBZSQJIOUXRYYEXWZCXXOAJMZVY', 
-            value: 1, 
-            //optional
-            message: 'Example message',
-            tag: 'TRYTETAG',
-            //indexes for input addresses, only in special cases required
-            // starIndex: 0,
-            // endIndex: 1
+    axios.post(url + "/payments", data)
+      .then(function (response) {
+        
+        console.log("result", response.status)
+        if(response.status === 200) {
+            console.log("body", response.data)
+            let open_payment = response.data.payment
+            let payoutObject = {
+                //required
+                address: open_payment.address, 
+                value: open_payment.value, 
+                //optional
+                message: 'Pay for energy',
+                tag: 'TRYTETAG'
+            }
+            paymentModule.payout.send(payoutObject)
+            .then(result => {
+                console.log("result", result)
+            })
+            .catch(err => {
+                console.log(err)
+            })
         }
-        paymentModule.payout.send(payoutObject)
-        .then(result => {
-            console.log("result", result)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    });
+      })
+      .catch(function (error) {
+          
+        console.log("ERRORORORORO");
+        console.log(error);
+      });
    
 
 }
 
-paymentModule.on('payoutSent', onPaymentSuccess);
+paymentModule.on('paymentSuccess', onPaymentSuccess);
 
 // Start server with iota-payment module on '/custom'
 server.listen(5000, function () {
     console.log(`Server started on http://localhost:5000 `)
 })
-
